9ca8d09d09e2445323250a87eaa4d10e14d16a71
Sharwin Takalkar <takalkarsharwin1342002@gmail.com>
2024-08-16 19:27:08 +0530

    pushed requirements and some other files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical bot',
+    version= '0.0.0',
+    author= 'sharwin takalkar',
+    author_email= 'takalkarsharwin.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
